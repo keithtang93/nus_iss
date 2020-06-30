41f22d48da3efeb4a186d57d07d60b118e87e6db
Owner <Owner@KeithTang>
2020-06-30 22:48:47 +0800

    2nd Commit (no Admin)

--- leave_management3/src/main/java/nus/iss/edu/leave/LeaveManagementApplication.java
+++ leave_management3/src/main/java/nus/iss/edu/leave/LeaveManagementApplication.java
@@ -21,28 +21,7 @@ import nus.iss.edu.leave.repo.EmployeeRepository;
 @SpringBootApplication
 public class LeaveManagementApplication {
 	
-/*	@Autowired
-	EmployeeRepository erepo;
-	
-	@DateTimeFormat(pattern="dd-MM-yyyy")
-	Date date;*/
-	
 	public static void main(String[] args) {
 		SpringApplication.run(LeaveManagementApplication.class, args);
 	}
-	
-	/*
-	 * @Bean public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-	 * return args ->{
-	 * 
-	 * Employee e1 = new Employee(); e1.setAddress("aaaa");
-	 * e1.setContact_no(95185999);
-	 * 
-	 * date = new Date ("01-02-1999"); e1.setDoh(date); e1.setName("sam");
-	 * e1.setPassword("123"); e1.setEmail("acbc@gmail.com");
-	 * 
-	 * };
-	 * 
-	 * }
-	 */
 }
--- leave_management3/src/main/java/nus/iss/edu/leave/repo/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package nus.iss.edu.leave.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import nus.iss.edu.leave.model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Integer> {
-
-}
